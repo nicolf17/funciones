4ab7721838d4f69b85b957a1b9b4841c1d8cd9b5
Nicolas Lopez <nicolaslopezforero17@gmail.com>
2025-03-16 22:32:03 -0500

    nuevo

--- 3.js
+++ 3.js
@@ -1,1 +1,27 @@
+let solicitarNumeroEntero = () => {
+    let num
+    do {
+        num = Number(prompt("Ingrese un número: "))
+        if (!Number.isInteger(num)) {
+            alert("El número debe ser un entero. Inténtelo de nuevo.")
+        }
+    } while (!Number.isInteger(num))
+    return num
+}
+
+let sumaDigitos = (num) => {
+
+    let suma = 0
+
+    while (num > 0) {
+        suma += num % 10 
+        num = Math.floor(num / 10)
+    }
+    return suma
+}
+
+
+solicitarNumeroEntero()
+
+alert(`La suma de los dígitos de ${num} es: ${sumaDigitos(num)}`)
 
